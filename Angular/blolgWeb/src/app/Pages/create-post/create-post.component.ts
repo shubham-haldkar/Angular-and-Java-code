39d63da1a645ca0db95347530bed74efdd33a2cf
Shubham-Haldkar <shubham.haldkar@gmail.com>
2025-09-23 22:49:56 +0530

    added form

--- Angular/blolgWeb/src/app/Pages/create-post/create-post.component.ts
+++ Angular/blolgWeb/src/app/Pages/create-post/create-post.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { MatChipsModule } from "@angular/material/chips";
+import { MatIcon } from '@angular/material/icon';
+import { NgForOf } from '@angular/common';
+import { MatFormField } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-create-post',
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, MatChipsModule, NgForOf, MatIcon, MatFormField ],
   templateUrl: './create-post.component.html',
   styleUrl: './create-post.component.css'
 })
 export class CreatePostComponent {
+  postForm!: FormGroup;
+  tags:string[]=[]; 
+$event: any;
+
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private snakbar: MatSnackBar
+  ) { }
+
+  ngOnInit() {
+    this.postForm = this.fb.group({
+      name: [null, Validators.required],
+      content: [null, Validators.required, Validators.maxLength(5000)],
+      img: [null, Validators.required],
+      postedBy: [null, Validators.required],
+    })
+  }
+
+  add(event:any){
+    const value = (event.value || '').trim();
+    if(value){
+      this.tags.push(value) ;
+    }
+
+    event.chipInput!.clear() ;
+  }
+
+  remove(tag:any){
+    const index = this.tags.indexOf(tag);
+    
+    if (index>=0) {
+      this.tags.splice(index,1) ;
+    }
+  }
 
 }
--- Angular/blolgWeb/src/app/nav-bar/nav-bar.component.ts
+++ Angular/blolgWeb/src/app/nav-bar/nav-bar.component.ts
@@ -1,8 +1,9 @@
-import { validateHorizontalPosition } from '@angular/cdk/overlay';
+ 
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+ 
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,21 +12,5 @@ import { Router } from '@angular/router';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-
-  postForm!: FormGroup;
-
-  constructor(private fb: FormBuilder,
-    private router : Router,
-    private snakbar : MatSnackBar
-  ){}
-
-  ngOnInit(){
-    this.postForm = this.fb.group({
-      name: [null,Validators.required],
-      content: [null,Validators.required, Validators.maxLength(5000) ],
-      img: [null,Validators.required],
-      postedBy: [null,Validators.required],
-    })
-  }
-
+    
 }
